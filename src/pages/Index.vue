2f5bf6530bbd6f7f553edc6148388a32f6772a07
Alan Jenkins <alanjenkins1978@gmail.com>
2019-04-14 12:12:56 +0100

    Stripped out old template content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,6 @@
 <template>
   <Layout>
-    
-    <!-- <g-image al`t="Example image" src="~/favicon.png" width="135" /> -->
-    
     <Portfolio/>
-    
-    <!-- <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p> -->
-
   </Layout>
 </template>
 
@@ -32,9 +17,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
