9ea85fab862336948d4d5913e047bdbd8d59de46
Alan Jenkins <alan.jenkins@visualsoft.co.uk>
2020-01-18 11:32:49 +0000

    Adds a custom 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout>
+    <div class="bg-white p-8">
+      <h1 class="text-5xl font-medium">OOPS!</h1>
+
+      <h1 class="text-3xl font-medium my-4">Sorry, this page isn't here.</h1>
+
+      <p class="leading-normal text-lg">
+        <a href="Javascript:history.back(-1)">Go back</a>
+        , or head over to the
+        <g-link to="/">Homepage</g-link>to choose another
+        direction
+      </p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Sorry, this page isn't here - Alan Jenkins"
+  }
+};
+</script>
